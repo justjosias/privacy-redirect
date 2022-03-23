248b5979f5d4a4252eec59d1fc5911d483a24d5e
Josias <me@josias.dev>
2022-03-23 12:37:34 -0700

    Add other simplytranslate instances

--- src/assets/javascripts/helpers/google-translate.js
+++ src/assets/javascripts/helpers/google-translate.js
@@ -1,6 +1,18 @@
 const targets = ["translate.google.com", "translate.google.no"];
 
-const redirects = ["https://simplytranslate.org"];
+const redirects = [
+  // From https://simple-web.org/instances/simplytranslate
+  "https://simplytranslate.org",
+  "https://st.alefvanoon.xyz",
+  "https://translate.josias.dev",
+  "https://translate.namazso.eu",
+  "https://translate.riverside.rocks",
+  "https://manerakai.asuscomm.com:447",
+  "https://translate.bus-hit.me",
+  "https://simplytranslate.pussthecat.org",
+  "https://translate.northboot.xyz",
+  "https://translate.tiekoetter.com"
+];
 
 export default {
   targets,
